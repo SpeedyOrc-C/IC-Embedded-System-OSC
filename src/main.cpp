9a675ce3d01517aff595929520a4cc6390b239b0
陈湛明 <chenzhanming04@foxmail.com>
2025-02-20 10:01:11 +0000

    tmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,174 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-//Constants
-  const uint32_t interval = 100; //Display update interval
+// Constants
+const uint32_t interval = 100; // Display update interval
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
-//Display driver object
+// Display driver object
 U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+    digitalWrite(REN_PIN, LOW);
+    digitalWrite(RA0_PIN, bitIdx & 0x01);
+    digitalWrite(RA1_PIN, bitIdx & 0x02);
+    digitalWrite(RA2_PIN, bitIdx & 0x04);
+    digitalWrite(OUT_PIN, value);
+    digitalWrite(REN_PIN, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(REN_PIN, LOW);
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    // put your setup code here, to run once:
 
-  //Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(REN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+    // Set pin directions
+    pinMode(RA0_PIN, OUTPUT);
+    pinMode(RA1_PIN, OUTPUT);
+    pinMode(RA2_PIN, OUTPUT);
+    pinMode(REN_PIN, OUTPUT);
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(OUTL_PIN, OUTPUT);
+    pinMode(OUTR_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
+    pinMode(C0_PIN, INPUT);
+    pinMode(C1_PIN, INPUT);
+    pinMode(C2_PIN, INPUT);
+    pinMode(C3_PIN, INPUT);
+    pinMode(JOYX_PIN, INPUT);
+    pinMode(JOYY_PIN, INPUT);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+    // Initialise display
+    setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
+    delayMicroseconds(2);
+    setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
+    u8g2.begin();
+    setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  //Initialise UART
-  Serial.begin(9600);
-  Serial.println("Hello World");
+    // Initialise UART
+    Serial.begin(9600);
+    Serial.println("Hello World");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
+enum KeyboardKey
+{
+    KeyC = 0,
+    KeyCs = 1,
+    KeyDf = 1,
+    KeyD = 2,
+    KeyDs = 3,
+    KeyEf = 3,
+    KeyE = 4,
+    KeyF = 5,
+    KeyFs = 6,
+    KeyGf = 6,
+    KeyG = 7,
+    KeyGs = 8,
+    KeyAf = 8,
+    KeyA = 9,
+    KeyAs = 10,
+    KeyBf = 10,
+    KeyB = 11,
+};
 
-  while (millis() < next);  //Wait for next interval
+bool TestKey(KeyboardKey key)
+{
+    digitalWrite(RA2_PIN, LOW);
 
-  next += interval;
+    switch (key)
+    {
+    case KeyC:
+    case KeyCs:
+    case KeyD:
+    case KeyDs:
+        digitalWrite(RA0_PIN, LOW);
+        digitalWrite(RA1_PIN, LOW);
+        break;
+    case KeyE:
+    case KeyF:
+    case KeyFs:
+    case KeyG:
+        digitalWrite(RA0_PIN, HIGH);
+        digitalWrite(RA1_PIN, LOW);
+        break;
+    case KeyGs:
+    case KeyA:
+    case KeyAs:
+    case KeyB:
+        digitalWrite(RA0_PIN, LOW);
+        digitalWrite(RA1_PIN, HIGH);
+        break;
+    }
 
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
+    switch (key)
+    {
+    case KeyC:
+    case KeyE:
+    case KeyGs:
+        return digitalRead(C0_PIN);
 
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
-  
+    }
+}
+
+KeyboardKey NextKeyOf(KeyboardKey key)
+{
+    if (key == KeyB)
+        return KeyC;
+
+    return (KeyboardKey)(((int)key) + 1);
+}
+
+void loop()
+{
+    static uint32_t next = millis();
+    static uint32_t count = 0;
+
+    while (millis() < next)
+        ;
+
+    next += interval;
+
+    u8g2.clearBuffer();
+    for (int key = 0; key <= 11; key += 1)
+    {
+        const bool isDown = TestKey((KeyboardKey)key);
+        const int x = key;
+
+        if (isDown)
+        {
+            u8g2.drawBox(x * 8, 0, 8, 8);
+        }
+    }
+    u8g2.sendBuffer();
 }
